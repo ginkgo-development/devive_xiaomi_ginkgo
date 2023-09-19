dbfe36cabc4f08ccdc3b35fcd9b148ee76b4d5b5
Cyber Knight <cyberknight755@gmail.com>
2023-09-20 02:23:11 +0800

    ginkgo: Import custom task_profiles from sunny
    - These are taken from [1] and modified for my usage.
    
    [1]: https://github.com/yaap/system_core/tree/thirteen/libprocessgroup/profiles/
    
    Change-Id: I4f7ef3be0dafa0c4f4c6d33bcaeac49ace910d9a
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: eklerismunir <munir.nasibzade@proton.me>

--- device.mk
+++ device.mk
@@ -409,6 +409,11 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
